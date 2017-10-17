54320fc2c1fdbf2a9168559b8c95fe6ac8de6ea7
Angeld55 <achi_d@abv.bg>
2017-10-17 18:49:58 +0300

    Create Eratosthenes.cpp

---
+++ Eratosthenes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+void Eratosthenes(unsigned n)
+{
+	bool* nums = new bool[n];
+	for (int i = 0; i < n; i++) //unnecessary
+		nums[i] = true;
+
+	for (int i = 2; i <n; i++)
+	{
+		if (nums[i])
+		{
+			int index = i+i;
+			while (index < n)
+			{
+				nums[index] = false;
+				index += i;
+			}
+		}
+	}
+	for (int i = 2; i < n; i++)
+	{
+		
+		if (nums[i])
+			std::cout << i<< " ";
+	}
+
+}
+int main() {
+	Eratosthenes(300);  // 2 3 5 7 11........... 191 193 197 199
+
+}
