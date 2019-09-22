669b9650880961d37020f2ed152fef0696b1a85a
Angeld55 <achi_d@abv.bg>
2019-09-22 12:04:04 +0300

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,1 +1,23 @@
-d
+void swap(int* arr,int i ,int j)
+{
+ int temp = arr[i];
+ arr[i]=arr[j];
+ arr[j]=temp;
+}
+void BubbleSort(int* arr,int len)
+{
+    for(int j = 0;j<len;j++)
+    {
+        bool isSwapped= false;
+        for(int i = 0;i<len-1-j;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+             swap(arr,i,i+1);
+             isSwapped=true;
+            }
+        }
+        if(!isSwapped)//sortiran l ie masiwa?
+            return;
+     }
+}
