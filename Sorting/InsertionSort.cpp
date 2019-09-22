7d268ddcdd529b21871a7b8b7029a9ef3fe414ec
Angeld55 <achi_d@abv.bg>
2019-09-22 12:04:59 +0300

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -1,1 +1,33 @@
-
+void swap(int* arr,int i ,int j)
+{
+ int temp = arr[i];
+ arr[i]=arr[j];
+ arr[j]=temp;
+}
+void NaiveInsertionSort(int* arr,int len) 
+{
+    for(int i = 1;i<len;i++)
+    {
+        int elIndex = i;
+        while(elIndex>0&&arr[elIndex]<arr[elIndex-1])
+        {
+         swap(arr,elIndex,elIndex-1);
+         elIndex--;
+        }
+         
+    }
+}
+void InsertionSort(int* arr,int len)
+{
+    for(int i =1;i<len;i++)
+    {
+        int element = arr[i];
+        int index = i-1;
+        while(index>=0&&arr[index]>element)
+        {
+            arr[index+1]=arr[index];
+            index--;
+        }
+        arr[index+1]=element;
+    }
+}
