cb1e5c431c1fa75f4057e50d21830c7b087e4429
Angeld55 <achi_d@abv.bg>
2019-09-22 12:34:32 +0300

    Update InsertionSort.cpp

--- Sorting/InsertionSort.cpp
+++ Sorting/InsertionSort.cpp
@@ -5,3 +5,32 @@ void swap(T* arr,T i ,T j)
  arr[i]=arr[j];
  arr[j]=temp;
 }
+template <class T>
+void NaiveInsertionSort(T* arr,int len) 
+{
+    for(int i = 1;i<len;i++)
+    {
+        T elIndex = i;
+        while(elIndex>0&&arr[elIndex]<arr[elIndex-1])
+        {
+         swap(arr,elIndex,elIndex-1);
+         elIndex--;
+        }
+         
+    }
+}
+template <class T>
+void InsertionSort(T* arr,int len)
+{
+    for(int i =1;i<len;i++)
+    {
+        T element = arr[i];
+        int index = i-1;
+        while(index>=0&&arr[index]>element)
+        {
+            arr[index+1]=arr[index];
+            index--;
+        }
+        arr[index+1]=element;
+    }
+}
