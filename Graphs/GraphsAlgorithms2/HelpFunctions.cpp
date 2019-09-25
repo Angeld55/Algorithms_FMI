d63d0be086d1b705e292f64d54ced3440dd7def4
Angeld55 <achi_d@abv.bg>
2019-09-25 21:08:35 +0300

    Update HelpFunctions.cpp

--- Graphs/GraphsAlgorithms2/HelpFunctions.cpp
+++ Graphs/GraphsAlgorithms2/HelpFunctions.cpp
@@ -51,3 +51,17 @@ void printArray(const int* arr, int len)
 	cout << endl;
 	cout << endl;
 }
+void setAllTo(bool* arr, int len, bool value)
+{
+	for (int i = 0; i < len; i++)
+		arr[i] = value;
+}
+bool allTrue(bool* arr, int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		if (!arr[i])
+			return false;
+	}
+	return true;
+}
