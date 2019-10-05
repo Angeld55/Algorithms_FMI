8c2df7b43097427aaadadd126ce916997a3e57f2
Angeld55 <achi_d@abv.bg>
2019-10-05 14:50:26 +0300

    Create main.cpp

---
+++ Graphs/GraphAlgorithms0/main.cpp
@@ -0,0 +1,20 @@
+#include "Graph.hpp"
+int main()
+{
+    Graph g(6);
+   
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(1,3);
+    g.addEdge(1,4);
+    g.addEdge(2,5);
+  
+    cout<<"BFS: ";
+    g.BFS();
+    cout<<endl;
+    
+    cout<<"DFS: ";
+    g.DFS();
+    cout<<endl;
+    
+}
