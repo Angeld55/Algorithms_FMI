f6d84b6069409a3151859b05d0789b6bd9317c8b
Angel Dimitriev <achi_d@abv.bg>
2021-03-10 12:18:31 +0200

    Update task09.cpp

--- Sem. 03/time_complexity_tasks_2/task09.cpp
+++ Sem. 03/time_complexity_tasks_2/task09.cpp
@@ -2,20 +2,20 @@
 #include <algorithm>
 
 
-int funcHelp(int i, int j)
+unsigned long long funcHelp(unsigned long long i, unsigned long long j)
 {
 	if (i == 0 && j == 0)
 		return 1;
 
 	if (i == 0)
-		return funcHelp(i,j-1) + funcHelp(i,j-1);
+		return funcHelp(i, j - 1) + funcHelp(i, j - 1);
 	if (j == 0)
 		return funcHelp(i - 1, j) + funcHelp(i-1, j);
 
 	return  10 * funcHelp(i - 1, j) +  11 * (i, j - 1) ;
 }
 
-int task9(int n)
+unsigned long long task9(unsigned long long n)
 {
 	return funcHelp(n, n);
 }
