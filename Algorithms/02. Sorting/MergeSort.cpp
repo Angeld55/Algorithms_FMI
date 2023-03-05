c8c77c2bc457889118ef359e9f08f6498a69a7bc
Angel Dimitriev <achi_d@abv.bg>
2023-03-05 02:04:14 +0200

    Update MergeSort.cpp

--- Algorithms/02. Sorting/MergeSort.cpp
+++ Algorithms/02. Sorting/MergeSort.cpp
@@ -1,62 +1,62 @@
 #include <iostream>
+using namespace std;
 
 
-template <class T> void MergeSort(T* arr, int len);
+template <class T>
+void Merge(T* firstArr, size_t firstSize, T* secondArr, size_t secondSize, T* resultArr)
+{
+	size_t resultIter = 0; 
+	size_t firstIter = 0;
+	size_t secondIter = 0;
 
-template <class T> void Merge(T* arr1,int len1,T* arr2,int len2);
+	while(firstIter < firstSize && secondIter < secondSize)
+		resultArr[resultIter++] = (firstArr[firstIter] < secondArr[secondIter] ? firstArr[firstIter++] : secondArr[secondIter++]);
+
+	while(firstIter < firstSize)
+		resultArr[resultIter++] = firstArr[firstIter++];
+
+	while(secondIter < secondSize)
+		resultArr[resultIter++] = secondArr[secondIter++];
+}
+
+template <class T>
+void MergeSortStep(T* pArr, size_t Size, T* pBuffer)
+{
+	if(Size <= 1)
+		return;
+
+	size_t middle = Size / 2;
+
+	MergeSortStep(pArr, middle, pBuffer);
+
+	MergeSortStep(pArr + middle, Size - middle, pBuffer + middle);
+
+	Merge(pArr, middle, pArr + middle, Size - middle, pBuffer);
+
+	for(size_t i = 0; i < Size; i++)
+		pArr[i] = pBuffer[i];
+}
+
+template <class T>
+void MergeSort(T* pArr, size_t Size)
+{
+	if (!pArr || Size == 0)
+		return;
+
+	T* pBuffer = new T[Size];
+
+	MergeSortStep(pArr, Size, pBuffer);
+
+	delete[]  pBuffer;
+}
 
 
-
+const int SIZE = 15;
 int main()
 {
- int arr[] = {9,8,7,6,5,4,3,2,1};
- 
- MergeSort<int>(arr,9);
- for(int i = 0; i<9; i++)
-    std::cout<<arr[i]<<" " ;
-    
-}
-template <class T>
-void MergeSort(T* arr, int len) 
-{
-   
-    if(len<=1)
-        return;
-        
-    int mid = len/2;
+	int arr1[] = { 15,14,13,12,11,30,90,8,7,6,5,4,3,2,1};
+	MergeSort(arr1, SIZE);
 
-    MergeSort(arr, mid);    
-    MergeSort(arr + mid, len - mid);
-  
-    Merge<T> (arr, mid, arr + mid, len - mid);
- 
-    
-}
-template <class T>
-void  Merge(T* arr1, int len1, T* arr2, int len2)
-{
- 
-    T* result = new T[len1 + len2];
-    int cursor1=0;
-    int cursor2=0;
-    int resultCursor=0;
-	
-    while(cursor1 < len1 && cursor2 < len2)
-    {
-        if(arr1[cursor1] < arr2[cursor2])
-            result[resultCursor++] = arr1[cursor1++];
-        else  
-            result[resultCursor++] = arr2[cursor2++];
-    }
-    while(cursor1 < len1)
-        result[resultCursor++]=arr1[cursor1++];
-
-    
-    while(cursor2 < len2)
-        result[resultCursor++]=arr2[cursor2++];
-        
-    for(int i = 0;i < len1+len2; i++)//Въпреки,че arr1 е с по-малка големина, можем да си ползволим това да излезем извън масива, понеже знаем, че arr1 с arr2 са слепени
-        arr1[i] = result[i];
-    delete[] result;
-    
+	for (int i = 0; i < SIZE; i++)
+		cout << arr1[i] << " ";
 }
